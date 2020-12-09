eb226e112953391f114e0b5ae4c276b1bf42d95d
Darek <das@umbraco.dk>
2020-12-09 20:19:45 +0100

    Added libraries and cleaned up the default stuff

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+      
+
+    <button class="btn btn-primary">Don't</button>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/main.scss'
+import 'bootstrap'
+import 'popper.js'
+import jQuery from 'jquery'
+
+window.$ = window.jQuery = jQuery
 
 Vue.config.productionTip = false
 
